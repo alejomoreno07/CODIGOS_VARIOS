11d5eb708cff18f67c0492d03c1945fa95425ca2
alejomoreno07 <alejomoreno@hotmail.com>
2016-01-24 20:10:09 -0500

    Update segment_tree.cpp

--- segment_tree.cpp
+++ segment_tree.cpp
@@ -1,5 +1,4 @@
-#include <bits/stdc++.h>
-using namespace std;
+
 
 class SegmentTree{
 public:
@@ -42,34 +41,3 @@ public:
 };
 
 
-//global variables
-int tc;
-int n,q;
-int x,l,r; // x: each element of the vector, l: left index from querie, r: right index from querie.
-SegmentTree  st;
-
-
-void start(int z){
-	printf("Case %d:\n",z+1);
-	scanf("%d %d",&n,&q);
-	st.A.clear();
-	for(int i=0;i<n;i++){
-		scanf("%d",&x);
-		st.A.push_back(x);
-	}
-	st.create();
-
-	for(int i=0;i<q;i++){
-		scanf("%d %d",&l,&r);
-		printf("%d\n",st.A[st.rmq(l-1,r-1)]);
-	}
-
-}
-
-
-int main(){
-	//ios_base::sync_with_stdio(false); cin.tie(NULL);
-	scanf("%d",&tc);
-	for(int i=0;i<tc;i++) start(i);
-	return 0;
-}
